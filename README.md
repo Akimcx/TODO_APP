f271ef328bf2dce7ccd1d9df3dd433e52b54551d
ACACIA Kassim A <kacacia24@gmail.com>
2020-04-13 15:46:30 -0400

    Redesigned of the app, now he look simpler

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# TODO_APPTest push
+# Akim's Todo App
+Inspired by the youtuber DevEd for the design
+I tried this befor watching his video (just to test my skills)
+Here's a link to his video
+https://www.youtube.com/wath?v=Ttf3CEsEwMQ
+My JS code isn't perfect but I'm planning to upgrade it.
+Also gonna implement the filter later
+
+# Issue
+-Chrome base browser
+    The icon to add not well placed - minor bug
--- js/index.js
+++ js/index.js
@@ -1,20 +1,87 @@
-const app = document.getElementById("app")
-const modal = document.getElementById("modal")
-const newItem = document.getElementById("newItem")
-const deleteItem = document.getElementById("deleteItem")
-const add = document.getElementById("add")
-const cancel = document.getElementById("cancel")
-modal.style.width = app.clientWidth / 2 + "px"
-modal.style.left = (app.clientWidth - modal.clientWidth) / 2 + "px"
+const newItem = document.getElementById("new-item")
+const addIcon = document.getElementById("fa")
+const todoList = document.getElementById("todo-list")
+let checksIcon = [...document.getElementsByClassName("check")]
+let trashes = [...document.getElementsByClassName("trash")]
 
-newItem.addEventListener("click", e => {
-    e.preventDefault()
-    modal.classList.remove("see")
-    modal.classList.add("fade-in")
+addIcon.addEventListener("click",addItem)
+
+newItem.addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+        addItem() 
+    }
 })
 
-cancel.addEventListener("click", e => {
-    e.preventDefault()
-    modal.classList.remove("fade-in")
-    modal.classList.add("see")
-})
+function addListener() {
+    checksIcon.forEach(checkIcon => {
+        checkIcon.addEventListener("click", e => {
+           const targetElt = e.target
+           let liElt = targetElt.parentElement
+           
+            if (liElt.nodeName !== "LI") {
+                liElt = liElt.parentElement
+            }
+    
+           liElt.classList.add("checked")
+           liElt.addEventListener("animationend", () => {
+               liElt.classList.add("done","deleted")
+               liElt.addEventListener("animationend", () =>{
+                   todoList.removeChild(liElt)
+               })
+           })
+        })
+    })
+    
+    trashes.forEach(trash => {
+        trash.addEventListener("click", e => {
+            const targetElt = e.target
+            let liElt = targetElt.parentElement
+            
+             if (liElt.nodeName !== "LI") {
+                 liElt = liElt.parentElement
+             }
+    
+             liElt.classList.add("deleted")
+               liElt.addEventListener("animationend", () =>{
+                   todoList.removeChild(liElt)
+               })
+        })
+    })
+}
+
+
+function createItem(itemValue) {
+    const li = document.createElement("li")
+    const spanCheckIcon = document.createElement("span")
+    const spanTrashIcon = document.createElement("span")
+    const checkIcon = document.createElement("i")
+    const trashIcon = document.createElement("i")
+
+    li.classList.add("todo-item")
+    li.innerText = itemValue
+
+    spanCheckIcon.classList.add("icon", "check")
+    checkIcon.classList.add("far","fa-check-circle")
+    spanCheckIcon.appendChild(checkIcon)
+    
+    spanTrashIcon.classList.add("icon", "trash")
+    trashIcon.classList.add("far","fa-trash-alt")
+    spanTrashIcon.appendChild(trashIcon)
+
+    li.appendChild(spanCheckIcon)
+    li.appendChild(spanTrashIcon)
+
+    return li
+}
+
+function addItem() {
+    const itemValue = newItem.value
+    if (itemValue) {
+        const li = createItem(itemValue)
+        todoList.appendChild(li)
+        newItem.value = ""
+        checksIcon = [...document.getElementsByClassName("check")]
+        trashes = [...document.getElementsByClassName("trash")]
+        addListener()
+    }
+}
